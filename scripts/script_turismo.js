3af3a6e41e04445bd0c217571fba9cea795bbc81
AstroNave <95354403+AstroNave76@users.noreply.github.com>
2023-10-19 08:55:10 -0300

    turismo
    animacion y validacion de formulario

--- scripts/script_turismo.js
+++ scripts/script_turismo.js
@@ -1,19 +1,51 @@
 function validarEnviar() {
-    // Validar el campo de nombre
-    if (document.fvalida.nombre.value.length <= 5) {
-        alert("Tiene que escribir su nombre");
-        document.fvalida.nombre.focus();
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var interes = document.getElementById("interes").value;
+    var consulta = document.getElementById("consulta").value;
+    
+    var errorNombre = document.getElementById("error-nombre");
+    var errorEmail = document.getElementById("error-email");
+    var errorInteres = document.getElementById("error-interes");
+    var errorConsulta = document.getElementById("error-consulta");
+    
+    // Reiniciamos los mensajes de error
+    errorNombre.textContent = "";
+    errorEmail.textContent = "";
+    errorInteres.textContent = "";
+    errorConsulta.textContent = "";
+    
+    // Validación del nombre (no debe estar vacío)
+    if (nombre.trim() === "") {
+        errorNombre.textContent = "Por favor, ingrese su nombre.";
         return;
-    };
-
-
-    if (document.fvalida.interes.selectedIndex == 0) {
-        alert("Debe seleccionar un motivo de su contacto.");
-        document.fvalida.interes.focus();
+    }
+    
+    // Validación del email
+    if (email.trim() === "" || !validarEmail(email)) {
+        errorEmail.textContent = "Por favor, ingrese un email válido.";
+        return;
+    }
+    // Validación del campo "interes" (debe ser diferente de "Elegir")
+    if (interes === "Elegir") {
+        errorInteres.textContent = "Por favor, seleccione una opción.";
         return;
     }
 
-    // Si todas las validaciones pasan, se envía el formulario.
-    alert("Muchas gracias por enviar el formulario");
-    document.fvalida.submit();
+    // Validación de la consulta (opcional: puedes agregar reglas de validación específicas)
+    if (consulta.trim() === "") {
+        errorConsulta.textContent = "Por favor, ingrese su consulta.";
+        return;
+    }
+    
+    // Si llegamos aquí, el formulario es válido y puedes enviar los datos.
+    // Puedes agregar la lógica de envío de datos aquí.
+    alert("Gracias "+ nombre + ",nos comunicaremos a la brevedad");
+
+    document.forms["fvalida"].reset();
+}
+
+function validarEmail(email) {
+    var emailRegExp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return emailRegExp.test(email);
 }
